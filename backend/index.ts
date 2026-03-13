139f0be315b1dcedce6934b997eeeec1fe9e0c99
keyember <dev.oudinallan@gmail.com>
2026-03-13 12:48:53 +0100

    Refactor Prisma configuration to use process.env.DATABASE_URL and improve error logging in index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -5,7 +5,10 @@ import cors from "cors";
 import "dotenv/config";
 
 const app = express();
+
+// Prisma 7 lira automatiquement ton fichier prisma.config.ts
 const prisma = new PrismaClient();
+
 const PORT = Number(process.env.PORT) || 3000;
 
 app.use(cors());
@@ -17,12 +20,14 @@ app.get("/", async (req, res) => {
     const allFruits = await prisma.devilFruit.findMany();
     res.json(allFruits);
   } catch (error) {
+    console.error(error); // Utile pour voir l'erreur exacte dans les logs Dokploy
     res
       .status(500)
       .json({ error: "Erreur lors de la récupération des fruits" });
   }
 });
 
+// Important : le "0.0.0.0" permet à Dokploy d'exposer le service correctement
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`🚀 Punk Records API lancée sur http://0.0.0.0:${PORT}`);
+  console.log(`🚀 Punk Records API lancée sur le port ${PORT}`);
 });
